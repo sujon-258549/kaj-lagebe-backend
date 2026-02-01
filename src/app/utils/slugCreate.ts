f715aafa8ba73ea6b715a0fe932faae4e44f25e3
sujon-258549 <sujon258549@gmail.com>
2026-02-01 08:31:37 +0600

    feat: introduce core Prisma schema for user, subscription, and content models, and add utilities for slug generation and user serial ID creation.

--- src/app/utils/slugCreate.ts
+++ src/app/utils/slugCreate.ts
@@ -1,5 +1,13 @@
 const slugCreate = (text: string) => {
-  return text.toLowerCase().replace(/ /g, "-");
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/[_\s]+/g, "-")
+    .replace(/[^a-z0-9\u0980-\u09FF-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
 };
 
 export default slugCreate;

f9046202588ea63948b659e4825a7e387ef4da34
sujon-258549 <sujon258549@gmail.com>
2026-02-02 07:21:05 +0600

    feat: Implement subscription module by adding its Prisma schema and API routes.

--- src/app/modules/subscription/subscription.routes.ts
+++ src/app/modules/subscription/subscription.routes.ts
@@ -9,6 +9,7 @@ router.post("/", SubscriptionControllers.createSubscription);
 router.get("/", SubscriptionControllers.getAllSubscription);
 router.get("/:id", SubscriptionControllers.getSubscriptionById);
 router.put("/:id", SubscriptionControllers.updateSubscription);
+router.patch("/:id/status", SubscriptionControllers.updateSubscriptionStatus);
 router.delete("/:id", SubscriptionControllers.deleteSubscription);
 
 export const SubscriptionRoutes = router;

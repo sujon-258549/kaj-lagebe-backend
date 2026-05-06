98d80a93d9d8c62ad55d7dbfdbc39cb0beb58a94
sujon-258549 <sujon258549@gmail.com>
2026-05-06 22:02:23 +0600

    feat: implement category routes for CRUD operations and status updates

--- src/app/modules/category/category.router.ts
+++ src/app/modules/category/category.router.ts
@@ -10,8 +10,8 @@ import { USER_ROLE } from "../users/user.constant.ts";
 const router = Router();
 
 router.post("/", auth(), validateRequest(CategoryValidation.createCategoryZodSchema), CategoryController.createCategory);
-router.get("/", auth(), CategoryController.getAllCategory);
-router.get("/:id", auth(), CategoryController.getCategoryByIdentifier);
+router.get("/", CategoryController.getAllCategory);
+router.get("/:id", CategoryController.getCategoryByIdentifier);
 router.put("/:id", auth(), validateRequest(CategoryValidation.updateCategoryZodSchema), CategoryController.updateCategory);
 router.patch("/:id/status", auth(), CategoryController.updateCategoryStatus);
 router.delete("/:id", auth(), CategoryController.deleteCategory);
